c2ff828653a7ee13828412a6994596ba53b65257
Atsushi Togo <atz.togo@gmail.com>
2023-06-01 15:25:46 +0900

    test: Add 3 small tests to run on github action

--- tests/conftest.py
+++ tests/conftest.py
@@ -62,6 +62,20 @@ def bs_sno2_223() -> np.ndarray:
 
 
 @pytest.fixture(scope="session")
+def bs_sno2_222() -> np.ndarray:
+    """Return basis sets of SnO2-222."""
+    ph = phonopy.load(cwd / "phonopy_SnO2_222_rd.yaml.xz", produce_fc=False)
+    sym_op_reps = SymOpReps(
+        ph.supercell.cell.T,
+        ph.supercell.scaled_positions.T,
+        ph.supercell.numbers,
+        log_level=1,
+    )
+    sbs = SymBasisSets(sym_op_reps.representations, log_level=1)
+    return sbs.basis_sets
+
+
+@pytest.fixture(scope="session")
 def bs_sio2_222() -> np.ndarray:
     """Return basis sets of SiO2-222."""
     ph = phonopy.load(cwd / "phonopy_SiO2_222_rd.yaml.xz", produce_fc=False)
@@ -76,9 +90,37 @@ def bs_sio2_222() -> np.ndarray:
 
 
 @pytest.fixture(scope="session")
+def bs_sio2_221() -> np.ndarray:
+    """Return basis sets of SiO2-221."""
+    ph = phonopy.load(cwd / "phonopy_SiO2_221_rd.yaml.xz", produce_fc=False)
+    sym_op_reps = SymOpReps(
+        ph.supercell.cell.T,
+        ph.supercell.scaled_positions.T,
+        ph.supercell.numbers,
+        log_level=1,
+    )
+    sbs = SymBasisSets(sym_op_reps.representations, log_level=1, lang="C")
+    return sbs.basis_sets
+
+
+@pytest.fixture(scope="session")
 def bs_gan_442() -> np.ndarray:
     """Return basis sets of GaN-442."""
     ph = phonopy.load(cwd / "phonopy_GaN_442_rd.yaml.xz", produce_fc=False)
+    sym_op_reps = SymOpReps(
+        ph.supercell.cell.T,
+        ph.supercell.scaled_positions.T,
+        ph.supercell.numbers,
+        log_level=1,
+    )
+    sbs = SymBasisSets(sym_op_reps.representations, log_level=1, lang="C")
+    return sbs.basis_sets
+
+
+@pytest.fixture(scope="session")
+def bs_gan_222() -> np.ndarray:
+    """Return basis sets of GaN-222."""
+    ph = phonopy.load(cwd / "phonopy_GaN_222_rd.yaml.xz", produce_fc=False)
     sym_op_reps = SymOpReps(
         ph.supercell.cell.T,
         ph.supercell.scaled_positions.T,
