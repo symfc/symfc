174dea52d7d0f16ca7840a8bd3a9513a16f72a99
Atsushi Togo <atz.togo@gmail.com>
2024-06-05 13:18:08 +0900

    Remove setup_requires from setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup
 setup(
     name="symfc",
     version="0.1",
-    setup_requires=["numpy", "setuptools"],
     description="This is the symfc module.",
     author="Atsuto Seko",
     author_email="seko@cms.mtl.kyoto-u.ac.jp",
