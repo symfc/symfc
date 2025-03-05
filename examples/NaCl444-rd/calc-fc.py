ce8483157a82eea4373cdd524b66390499a5bebd
Atsushi Togo <atz.togo@gmail.com>
2025-03-05 10:58:20 +0900

    Fix example

--- examples/NaCl444-rd/calc-fc.py
+++ examples/NaCl444-rd/calc-fc.py
@@ -9,10 +9,8 @@ symfc = Symfc(
     ph.supercell,
     displacements=ph.dataset["displacements"],
     forces=ph.dataset["forces"],
-    orders=[
-        2,
-    ],
     log_level=1,
-)
+).run(orders=[2])
+
 ph.force_constants = symfc.force_constants[2]
 ph.auto_band_structure(plot=True).show()
