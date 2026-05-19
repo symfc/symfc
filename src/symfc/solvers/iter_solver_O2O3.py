a26eaf7ec542bfb9523a7f2c4ce2718fe3f6ee66
Atsuto Seko <sekocha@gmail.com>
2026-05-19 23:12:18 +0900

    Implement Adam

--- src/symfc/solvers/iter_solver_O2O3.py
+++ src/symfc/solvers/iter_solver_O2O3.py
@@ -94,7 +94,7 @@ class FCIterSolverO2O3(FCSolverBase):
         fc2_basis: FCBasisSetO2 = cast(FCBasisSetO2, self._basis_set[0])
         fc3_basis: FCBasisSetO3 = cast(FCBasisSetO3, self._basis_set[1])
 
-        coefs = solve_sgd_O2O3(
+        coefs = solve_adam_O2O3(
             d,
             f,
             fc2_basis,
@@ -308,6 +308,163 @@ def solve_sgd_O2O3(
             print("RMSE:", rmse, flush=True)
 
         if np.abs(rmse - rmse_prev) < 1e-8:
+            break
+        rmse_prev = rmse
+
+    compress_eigvecs = _get_linked_compress_eigvecs(
+        fc2_basis.blocked_basis_set,
+        fc3_basis.blocked_basis_set,
+    )
+    coefs = compress_eigvecs.T @ coefs
+
+    fc2_basis.blocked_basis_set.reset_indices()
+    fc3_basis.blocked_basis_set.reset_indices()
+    compact_compress_mat_fc2 /= const_fc2
+    compact_compress_mat_fc3 /= const_fc3
+    t_all2 = time.time()
+    if verbose:
+        header = "Time (disp @ compr @ eigvecs).T @ (disp @ compr @ eigvecs):"
+        print(header, "{:.3f}".format(t_all2 - t_all1), flush=True)
+    return coefs
+
+
+def solve_adam_O2O3(
+    disps: np.ndarray,
+    forces: np.ndarray,
+    fc2_basis: FCBasisSetO2,
+    fc3_basis: FCBasisSetO3,
+    batch_size: int = 100,
+    n_epoch: int = 1000,
+    beta1: float = 0.9,
+    beta2: float = 0.999,
+    use_mkl: bool = False,
+    verbose: bool = False,
+):
+    r"""Calculate X.T @ X and X.T @ y.
+
+    X = displacements @ compress_mat @ compress_eigvecs
+    X = np.hstack([X_fc2, X_fc3])
+
+    displacements (fc2): (n_samples, N3)
+    displacements (fc3): (n_samples, NN33)
+    compact_compress_mat_fc2: (n_aN33, n_compr)
+    compact_compress_mat_fc3: (n_aNN333, n_compr_fc3)
+    compress_eigvecs_fc2: (n_compr_fc2, n_basis_fc2)
+    compress_eigvecs_fc3: (n_compr_fc3, n_basis_fc3)
+    Matrix reshapings are appropriately applied to compress_mat
+    and its products.
+
+    X.T @ X and X.T @ y are sequentially calculated using divided dataset.
+    X.T @ X = \sum_i X_i.T @ X_i
+    X.T @ y = \sum_i X_i.T @ y_i (i: batch index)
+    """
+    N3 = disps.shape[1]
+    N = N3 // 3
+
+    compact_compress_mat_fc2 = fc2_basis.compact_compression_matrix
+    compact_compress_mat_fc3 = fc3_basis.compact_compression_matrix
+    atomic_decompr_idx_fc2 = fc2_basis.atomic_decompr_idx
+    atomic_decompr_idx_fc3 = fc3_basis.atomic_decompr_idx
+
+    if compact_compress_mat_fc2 is None or compact_compress_mat_fc3 is None:
+        raise ValueError(
+            "Compression matrices or basis sets are not set. "
+            "Call run() method to compute them."
+        )
+
+    n_compr_fc2 = compact_compress_mat_fc2.shape[1]  # type: ignore
+    n_compr_fc3 = compact_compress_mat_fc3.shape[1]  # type: ignore
+
+    n_batch = (N // 100 + 1) * (n_compr_fc3 // 20000 + 1)
+    n_batch = min(N, n_batch)
+    begin_batch_atom, end_batch_atom = get_batch_slice(N, N // n_batch)
+    begin_batch, end_batch = get_batch_slice(disps.shape[0], batch_size)
+
+    n_compr = n_compr_fc2 + n_compr_fc3
+
+    t_all1 = time.time()
+    const_fc2 = -1.0
+    const_fc3 = -0.5
+    compact_compress_mat_fc2 *= const_fc2
+    compact_compress_mat_fc3 *= const_fc3
+
+    coefs = np.ones(n_compr)
+    learning_rate = 10
+
+    directions_prev = None
+    magnitudes_prev = None
+    rmse_prev = 1e10
+    for i_epoch in range(n_epoch):
+        if verbose:
+            print("-----", flush=True)
+            print("Epoch:", i_epoch + 1, flush=True)
+
+        error_all = []
+        for begin_i, end_i in zip(begin_batch_atom, end_batch_atom, strict=True):
+            if verbose:
+                print("-----", flush=True)
+                print("Solver_atoms:", begin_i + 1, "--", end_i, "/", N, flush=True)
+            n_atom_batch = end_i - begin_i
+
+            t1 = time.time()
+            compr_mat_fc2 = reshape_compr_mat_O2(
+                compact_compress_mat_fc2, atomic_decompr_idx_fc2, N, begin_i, end_i
+            )
+            compr_mat_fc3 = reshape_compr_mat_O3(
+                compact_compress_mat_fc3, atomic_decompr_idx_fc3, N, begin_i, end_i
+            )
+            t2 = time.time()
+            if verbose:
+                time_pr = "{:.3f}".format(t2 - t1)
+                print("Time (Solver_compr_matrix_reshape):", time_pr, flush=True)
+
+            # TODO: Use structure index permutation.
+            for begin, end in zip(begin_batch, end_batch, strict=True):
+                if verbose:
+                    print("Solver_block:", end, "/", disps.shape[0], flush=True)
+                t1 = time.time()
+                X = np.zeros((n_atom_batch * 3 * (end - begin), n_compr))
+                X[:, :n_compr_fc2] = dot_product_sparse(
+                    disps[begin:end],
+                    compr_mat_fc2,
+                    use_mkl=use_mkl,
+                    dense=True,
+                ).reshape((-1, n_compr_fc2))
+                X[:, n_compr_fc2:] = dot_product_sparse(
+                    set_disps_N3N3(disps[begin:end], sparse=False),
+                    compr_mat_fc3,
+                    use_mkl=use_mkl,
+                    dense=True,
+                ).reshape((-1, n_compr_fc3))
+                y = forces[begin:end, begin_i * 3 : end_i * 3].reshape(-1)
+
+                error = X @ coefs - y
+                grad = X.T @ error
+                error_all.extend(error)
+
+                if directions_prev is None:
+                    directions = grad
+                    magnitudes = grad**2
+                else:
+                    directions = beta1 * directions_prev + (1 - beta1) * grad
+                    magnitudes = beta2 * magnitudes_prev + (1 - beta2) * (grad**2)
+                normalized_directions = directions / np.sqrt(magnitudes)
+
+                coefs -= learning_rate * normalized_directions
+
+                directions_prev = directions
+                magnitudes_prev = magnitudes
+
+            t2 = time.time()
+            if verbose:
+                print(" - Time:", "{:.3f}".format(t2 - t1), flush=True)
+
+        error_all = np.array(error_all)
+        rmse = np.sqrt(np.mean(error_all**2))
+        if verbose:
+            print("RMSE:", rmse, flush=True)
+
+        if np.abs(rmse - rmse_prev) < 1e-9:
             break
         rmse_prev = rmse
 
