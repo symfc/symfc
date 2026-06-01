6ed2c427bec8afbaff70da7a0720ac074f2c2eae
Atsuto Seko <sekocha@gmail.com>
2026-06-01 11:08:39 +0900

    Implementing combinations

--- src/symfc/utils/permutation_tools_O4.py
+++ src/symfc/utils/permutation_tools_O4.py
@@ -351,7 +351,7 @@ class PermutationO4:
         perm_decompr_idx = self._run_indep3(perm_decompr_idx, n_batch=n_batch3)
         perm_decompr_idx = self._run_indep4(perm_decompr_idx, n_batch=n_batch4)
 
-        if natom <= 128:
+        if natom <= 300:
             # if natom <= 50:
             self._convert_to_matrix(perm_decompr_idx)
         else:
